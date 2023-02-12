d5c9b3687a9a6e7e6f00994254ff790855e7f2cb
ana94-ana <aniabramishvili94@gmail.com>
2023-02-12 17:25:03 +0530

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <script setup>
 import Cart from './components/Cart.vue'
 import Products from './components/Products.vue'
-import Search from './components/Search.vue'
 
+const menu=[
+  {routeName:'Home', title: 'Main Page'},
+  {routeName:'About', title: 'About Us'},
+  {routeName:'Contact', title: 'Contact Us'}
+]
 
 </script>
 
 <template>
 
-<div class="container flex flex-row mt-4 mx-auto items-center">
-  <Products />
-  <Cart/>
+<div class="container flex flex-col mt-4 mx-auto space-y-4">
+ <!--<Products />
+  <Cart/>-->
+  <div class="flex flex-row space-x-4">
+    <RouterLink v-for="item in menu" :key="item.index" :to="{name: item.routeName}">{{ item.title }}</RouterLink>
+
+   <!-- <RouterLink to="/">Home Page</RouterLink>
+  <RouterLink to="/about">About Page</RouterLink>
+  <RouterLink to="/contact">Contact Page</RouterLink>-->
+
+  </div>
+  <div>
+    <RouterView/>
+  </div>
 </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,4 +4,38 @@ import App from './App.vue'
 import store from './store'
 
 
-createApp(App).use(store).mount('#app')
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+import Contact from './views/Contact.vue'
+
+
+
+const routes = [
+    {
+        path: '/',
+        name: "Home",
+        component: Home
+    },
+    {
+        path: '/about',
+        name: "About",
+        component: About
+    },
+    {
+        path: '/contact',
+        name: "Contact",
+        component: Contact
+    }
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: routes
+})
+
+
+createApp(App)
+    .use(store)
+    .use(router)
+    .mount('#app')
