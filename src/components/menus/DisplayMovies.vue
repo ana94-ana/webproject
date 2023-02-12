1e1344418aba126d3082f4f571edeec5640b619a
ana94-ana <aniabramishvili94@gmail.com>
2023-02-13 01:37:41 +0530

    add cards tailwind

--- src/components/menus/DisplayMovies.vue
+++ src/components/menus/DisplayMovies.vue
@@ -9,8 +9,8 @@ const route = useRoute()
 const store = useStore()
 
 const movi = computed(() => {
-    
-    store.dispatch('getRecordById',route.params.id)
+
+    store.dispatch('getRecordById', route.params.id)
     return store.getters.getactiveMovies
 
 })
@@ -18,7 +18,16 @@ const movi = computed(() => {
 </script>
 
 <template>
-    <div>
-        {{ movi }}
+
+    <div class="max-w-sm rounded overflow-hidden shadow-lg mg-left">
+        <img class="w-full" :src="movi.cover">
+        <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">{{ movi.name }}</div>
+            <p class="text-gray-700 text-base">
+                {{ movi.description }}
+            </p>
+        </div>
+
     </div>
+
 </template>
