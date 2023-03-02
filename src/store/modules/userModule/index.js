1248b3f5989b7631ca7fc62bf922159180b2a665
ana94_ana <aniabramishvili94@gmail.com>
2023-03-02 21:19:36 +0530

    new file:   src/store/modules/userModule/index.js 	modified:   src/router/routes.js 	modified:   src/store/plugins/mainPlugin.js 	modified:   src/views/account/AccountInfo.vue 	modified:   src/views/account/AccountUpdate.vue

---
+++ src/store/modules/userModule/index.js
@@ -0,0 +1,64 @@
+const userModule = {
+    namespased: true,
+    state() {
+        return {
+            autentificated: null,
+            users: [
+                {
+                    id: 1,
+                    name: 'natali',
+                    email: 'natali@gmail.com',
+                    password: 'natali'
+                },
+                {
+                    id: 2,
+                    name: 'nanuka',
+                    email: 'nanuka@gmail.com',
+                    password: 'nanuka'
+                },
+                {
+                    id: 3,
+                    name: 'natali3',
+                    email: 'natali3@gmail.com',
+                    password: 'natali3'
+                }
+            ]
+        }
+    },
+    getters: {
+        userAutentificated(state) {
+            return state.autentificated
+        }
+    },
+    mutations: {
+        AUTENTIFICATE(state, payload) {
+            let user = state.user.filter(value => value.email === payload.email && user.password === payload.password)
+            if (user) {
+                state.autentificated = user
+            }
+
+        },
+        REGISTER_USER(state, payload) {
+            state.user.push(payload)
+        },
+        UPDATE_USER(state, payload) {
+            state.users.find(user => state.autentificated.id === user.id).name = payload.name
+            state.users.find(user => state.autentificated.id === user.id).email = payload.email
+            state.users.find(user => state.autentificated.id === user.id).password = payload.password
+        }
+
+    },
+    actions: {
+        autentificate({ commit }, payload) {
+            commit('AUTENTIFICATE', payload)
+        },
+        registration({ commit }, payload) {
+            commit('REGISTER_USER', payload)
+        },
+        updateUser({ commit }, payload) {
+            commit('UPDATE_USER', payload)
+        }
+
+    }
+}
+export default userModule
