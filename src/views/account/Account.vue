0f3ea0fca747ed9e8af440930d7fd5aa27351460
ana94_ana <aniabramishvili94@gmail.com>
2023-03-02 18:16:31 +0530

    error routes

--- src/views/account/Account.vue
+++ src/views/account/Account.vue
@@ -2,10 +2,13 @@
 
 </script>
 <template>
-<div>Account</div>
-<ul>
-    <li><RouterLink :to="{name:'AccountInfo'}">User Info</RouterLink> </li>
-    <li><RouterLink :to="{name:'AccountUpdate'}">User Update</RouterLink> </li>
-</ul>
-<RouterView/>
-</template>
+    <div>Account</div>
+    <ul>
+        <li>
+            <RouterLink :to="{ name: 'AccountInfo', params: { userId: 1 } }">User Info</RouterLink>
+        </li>
+        <li>
+            <RouterLink :to="{ name: 'AccountUpdate', params: { userId: 1 } }">User Update</RouterLink>
+        </li>
+    </ul>
+<RouterView /></template>
