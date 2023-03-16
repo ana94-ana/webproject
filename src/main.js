2e27dc36375a7680b054d61bf0533939234a5253
ana94_ana <aniabramishvili94@gmail.com>
2023-03-16 18:47:15 +0530

    add token

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ axios.defaults.baseURL=import.meta.env.VITE_BASE_API_URL
 axios.defaults.headers['Accept'] = 'Application/json'
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + JSON.parse(localStorage.getItem("token"))
 
+console.log(axios.defaults)
 createApp(App)
     .use(store)
     .use(router)
